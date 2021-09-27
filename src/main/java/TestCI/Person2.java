49e14ec98036155f9b062915a02cd206986e8ca9
idiattara <74354003+idiattara@users.noreply.github.com>
2021-09-27 09:47:19 +0200

    Update Person2.java

--- src/main/java/TestCI/Person2.java
+++ src/main/java/TestCI/Person2.java
@@ -3,7 +3,7 @@ public class Person2 {
     String name;
     int age;
 
-    public Person(String name, int age)
+    public Person2(String name, int age)
     {
         this.name = name;
         this.age = age;
