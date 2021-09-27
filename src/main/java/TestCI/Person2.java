7ffbb49ade546340faf77f2764b56d5cbf50b1eb
idiattara <74354003+idiattara@users.noreply.github.com>
2021-09-27 09:44:36 +0200

    Create Person2.java

---
+++ src/main/java/TestCI/Person2.java
@@ -0,0 +1,21 @@
+ackage TestCI;
+public class Person2 {
+    String name;
+    int age;
+
+    public Person(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+}
