4aec1ec4f517bcd63e70ae43cca58141be270245
idiattara <74354003+idiattara@users.noreply.github.com>
2021-09-27 14:36:31 +0200

    Update Main.java

--- src/main/java/TestCI/Main.java
+++ src/main/java/TestCI/Main.java
@@ -4,8 +4,6 @@ public class Main {
         {
         Person person = new Person(args[0] ,Integer.parseInt(args[1]));
         System.out.println(person.getAge());
-                Person2 person2 = new Person2(args[0] ,Integer.parseInt(args[1]));
-        System.out.println(person2.getAge());
         }
 }
 
